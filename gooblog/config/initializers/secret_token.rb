146479a09b6db1858ea43f06c9ae49ce796fc534
bbungtygy <bbungtygy@gmail.com>
2014-04-11 02:09:44 +0900

    0.1 rails new gooblog

---
+++ gooblog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gooblog::Application.config.secret_key_base = 'ae1f342c67954468ffaa39c2fcdf1c1b752f93ddb9a586acc64aafe90cb6006bed02f1ced0893e22cf76ec0d4302a15ec1c17185a43c24831f54127807e7c249'
