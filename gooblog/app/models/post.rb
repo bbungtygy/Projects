eca51e96b2e95e5047b5ee5cf04ede45b41526a9
bbungtygy <bbungtygy@gmail.com>
2014-04-11 03:00:52 +0900

    1.2 posts

--- gooblog/app/models/post.rb
+++ gooblog/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  validates :title, :content, presence: :true
 
-  has_attached_file :asset, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :asset, :styles => { :main => "600x600>", :thumb => "100x100!" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :asset, :content_type => /\Aimage\/.*\Z/
 end
