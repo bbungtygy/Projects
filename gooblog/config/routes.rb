9fb4747d0c481100cdcecb52b039beb082c36bb7
bbungtygy <bbungtygy@gmail.com>
2014-04-11 02:12:49 +0900

    0.2 rails g controller

--- gooblog/config/routes.rb
+++ gooblog/config/routes.rb
@@ -1,4 +1,10 @@
 Gooblog::Application.routes.draw do
+
+  resources :posts
+  resources :comments
+
+  root to: "posts#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
