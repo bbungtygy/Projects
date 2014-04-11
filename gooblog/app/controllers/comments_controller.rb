42015d90821dfc9ac1e98547a4111f6dc6bff742
bbungtygy <bbungtygy@gmail.com>
2014-04-11 14:51:52 +0900

    1.3 comments

--- gooblog/app/controllers/comments_controller.rb
+++ gooblog/app/controllers/comments_controller.rb
@@ -1,2 +1,49 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+
+    if @comment.save
+      redirect_to @comment.post
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to @comment.post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    @comment.destroy
+    redirect_to @comment.post 
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :content, :post_id)
+  end
 end
--- gooblog/app/models/comment.rb
+++ gooblog/app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :commenter, :content, :post_id, presence: :true
 end
